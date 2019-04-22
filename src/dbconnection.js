4f01fdc9632df88a6df6d4f58d1bf57f5972b740
Willians Vargas <williant.marquez@gmail.com>
2019-04-21 22:42:11 -0500

    Conexion arreglada.

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -5,7 +5,6 @@ let options = {
 
 let pgp = require('pg-promise')(options);
 
-desarrollo
 const  urlconnection = 'postgres://modulo4:modulo4d@159.65.230.188:5432/tcs2';
 
 let cn = pgp(urlconnection);
